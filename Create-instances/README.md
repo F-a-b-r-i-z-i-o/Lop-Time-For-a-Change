2eb31ace0eec275bb3bcd3b88dbbd310c245eac5
F-a-b-r-i-z-i-o <fabriziofagiolo18@gmail.com>
2026-01-03 16:07:03 +0100

    new results log test and file for generate dataset

--- Create-instances/README.md
+++ Create-instances/README.md
@@ -13,3 +13,8 @@
  RU, AU, CH, TR, TW, NO, ID, ZA, WA, WL, WE, WF,
  WM
  ]
+
+### Examples - another way see help command
+
+python3 construct_dataset.py --file_path ../Compact-data/IOT_2022_pxp.zip -t A --r AT BE BG CY CZ DE DK EE ES FI FR GR HR HU IE IT LT LU LV MT NL PL PT RO SE SI SK GB US JP CN CA KR BR IN MX RU AU CH TR TW NO ID ZA WA WL WE WF WM --out-dir ../pxp_n200 --out-files-names "pxp_{regions}_n200
+
--- Create-instances/construct_dataset.py
+++ Create-instances/construct_dataset.py
@@ -1,42 +1,37 @@
 import pymrio
 import numpy as np
 import argparse
-import os 
+import os
 
 class LoadInstance:
     def __init__(self, file_path: str) -> None:
-        """
-        Load the EXIOBASE3 IO system once.
-        """
+        """Load the EXIOBASE3 IO system once."""
         self.file_path = file_path
         self.matrix = self._load_matrix()
-    
+
     def _load_matrix(self):
-        """
-        loads the EXIOBASE3 system.
-        """
+        """loads the EXIOBASE3 system."""
         exio_matrix = pymrio.parse_exiobase3(self.file_path)
         return exio_matrix
 
-    def construct_sub_matrix(self, region: str, type_of_matrix_input: str) -> np.array: 
+    def construct_sub_matrix_regions(self, region: str, type_of_matrix_input: str) -> np.array:
         """
-        Create a square numeric sub-matrix for one region and save it as
-        a CSV with NO index, NO header (so no leading comma).
+        Create a square numeric sub-matrix for one region.
         """
-        # choose the matrix type: Z, Y, A, ...
         base_matrix = getattr(self.matrix, type_of_matrix_input)
         rows_region = base_matrix.xs(region, level='region', axis=0)
         cols_region = rows_region.xs(region, level='region', axis=1)
 
-        # Convert to pure numpy array: no index, no header, just numbers
         numeric = cols_region.to_numpy(dtype=np.float128, copy=True)
-        filename = f"sub_matrix_{region}_{type_of_matrix_input}"
-        
-        if np.isnan(numeric).any() or np.isinf(numeric).any():
-            raise ValueError("Sub-matrix contains NaN/Inf values.")
-        
-                
-        return filename, numeric
+
+        SCALE = np.int64(1000000000000000)
+        numeric *= SCALE
+
+        if np.isnan(numeric).any():
+            raise ValueError("Sub-matrix contains NaN values.")
+
+        return numeric
+    
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -58,38 +53,56 @@ def parse_args():
         dest="types_of_matrix",
         nargs="+",
         default=["z"],
-        help="Matrix types to extract (e.g. Y-Z-x). Default: Z",
+        help="Matrix types to extract (e.g. z y a x). Default: z",
     )
     parser.add_argument(
-        "-o", "--out-dir",
+        "--out-dir",
         dest="out_dir",
-        required=True,
-        help="Folder to save the Dataset of sub-matrix",
+        default="out_submatrices",
+        help="Folder to save the dataset of sub-matrices (default: out_submatrices).",
+    )
+    parser.add_argument(
+        "--out-files-names",
+        dest="output_name_template",
+        default="sub_matrix_{regions}_{mtype}.txt",
+        help=(
+            "Template for output filename. You can use placeholders: "
+            "Default: sub_matrix_{regions}_{mtype}.txt"
+        ),
     )
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     args = parse_args()
-    out_dir = args.out_dir
 
     print("--- Start Load Matrix ---")
     instance = LoadInstance(args.file_path)
     print("--- Matrix Loaded ---")
 
-    os.makedirs(out_dir, exist_ok=True)
+    os.makedirs(args.out_dir, exist_ok=True)
 
     for region in args.regions:
         for mtype in args.types_of_matrix:
-            filename, numeric = instance.construct_sub_matrix(region, mtype)
-            saved_path = os.path.join(out_dir, filename)  
-            header = str(numeric.shape[0])               
+            numeric = instance.construct_sub_matrix_regions(region, mtype)
+
+            rows, cols = numeric.shape
+            filename = args.output_name_template.format(
+                regions=region,
+                mtype=mtype,
+                rows=rows,
+                cols=cols
+            )
+
+            saved_path = os.path.join(args.out_dir, filename)
+            header = str(rows)
             np.savetxt(
                 saved_path,
                 numeric,
-                fmt="%.15g",
-                delimiter=" ",   
+                fmt="%d",
+                delimiter=" ",
                 header=header,
-                comments=""     
+                comments=""
             )
 
             print(f"Saved: {saved_path} (shape={numeric.shape}), name={filename}")
