68c81ecc2a6f4cf1acdbd71b0289ddf6616854db
F-a-b-r-i-z-i-o <fabriziofagiolo18@gmail.com>
2026-01-08 10:42:28 +0100

    fix name file

--- Create-instances/create_dataset.sh
+++ Create-instances/create_dataset.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for s in construct_dataset_aggregate_category.py construct_dataset_aggregate_code.py construct_dataset_aggregate_regions construct_dataset_regions; do
+for s in agg_cat.py agg_code.py agg_regions.py sub_regions.py; do
   echo "== Create $s =="
   python "$s"
 done
