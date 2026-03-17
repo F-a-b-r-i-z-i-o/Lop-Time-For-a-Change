b9b4d4ead016d6ea5eafe98e87a281f04c120c42
F-a-b-r-i-z-i-o <fabriziofagiolo18@gmail.com>
2026-03-17 17:21:46 +0100

    fix isic analysis

--- analize_ss_results.py
+++ analize_ss_results.py
@@ -89,38 +89,6 @@ plt.close()
 
 
 # =========================
-# Isic lineplot
-# =========================
-
-df_isic = pd.read_pickle("results/isic_exp2.pickle")
-
-df_plot = df_isic.copy()
-df_plot["instance"] = "isic"
-df_plot = df_plot[df_plot["nopt"] > 0]
-g = sns.relplot(
-    data=df_plot,
-    x="sparsity",
-    y="nopt",
-    kind="line",
-    hue="instance",
-    palette=["tab:red"],
-    legend=True,
-)
-ax = g.ax
-ax.set(xlabel="Sparsity", ylabel="Numbers of Optima", yscale="log")
-#ax.ticklabel_format(axis="both", style="plain", useOffset=False)
-
-sns.move_legend(g, "upper center", title="Instance")
-
-
-g.figure.tight_layout()
-g.figure.savefig("fig_isic_lineplot.pdf", bbox_inches="tight")
-#plt.show()
-plt.close(g.figure)
-
-
-
-# =========================
 # mann_whitney TEST
 # =========================
 
